2364c2dd99f6c71bbca39994e34b420c8edb731d
dcerisano <dcerisano@dino>
2017-10-21 20:04:39 -0400

    Signed-off-by: dcerisano <dcerisano@dino>

---
+++ install.sh
@@ -0,0 +1,15 @@
+sudo apt install rustc cargo
+cargo --cargo build --release
+
+sudo cp ./rgb-cpu-thruster.service /etc/systemd/system
+sudo chown root /target/release/msi-rgb
+sudo chmod u+s ./target/release/msi-rgb
+sudo cp ./target/release/msi-rgb   /usr/local/bin
+sudo cp ./rgb-cpu-thruster.sh      /usr/local/bin
+sudo cp ./rgb-sound-visualizer.sh      /usr/local/bin
+
+sudo systemctl enable rgb-cpu-thruster
+sudo systemctl start  rgb-cpu-thruster
+
+# systemctl --user enable rgb-sound-visualizer
+# systemctl --user start  rgb-sound-visualizer
