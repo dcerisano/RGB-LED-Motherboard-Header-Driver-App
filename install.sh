9e52226540fcb0f5bc3063c2ef2bcb1abb1923ca
dcerisano <dcerisano@dino>
2017-10-21 20:15:00 -0400

    Signed-off-by: dcerisano <dcerisano@dino>

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 sudo apt install rustc cargo make
 cargo build --release
 
-sudo cp ./rgb-cpu-thruster.service /etc/systemd/system
-sudo chown root /target/release/msi-rgb
-sudo chmod u+s ./target/release/msi-rgb
-sudo cp ./target/release/msi-rgb   /usr/local/bin
-sudo cp ./rgb-cpu-thruster.sh      /usr/local/bin
-sudo cp ./rgb-sound-visualizer.sh      /usr/local/bin
-
+sudo cp         ./rgb-cpu-thruster.service   /etc/systemd/system
+sudo chown root ./target/release/msi-rgb
+sudo chmod u+s  ./target/release/msi-rgb
+sudo cp         ./target/release/msi-rgb     /usr/local/bin
+sudo cp         ./rgb-cpu-thruster.sh        /usr/local/bin
+sudo cp         ./rgb-sound-visualizer.sh    /usr/local/bin
+       
 sudo systemctl enable rgb-cpu-thruster
 sudo systemctl start  rgb-cpu-thruster
 
