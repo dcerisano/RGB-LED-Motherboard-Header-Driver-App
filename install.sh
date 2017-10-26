18c26d6c9cc4f0c72dc246554bd2764b1764b8d2
dcerisano <dcerisano@dino>
2017-10-25 21:02:26 -0400

    Signed-off-by: dcerisano <dcerisano@dino>

--- install.sh
+++ install.sh
@@ -8,12 +8,14 @@ cargo build --release
 sudo cp         ./rgb-cpu.service     /etc/systemd/system
 sudo cp         ./rgb-net.service     /etc/systemd/system
 sudo cp         ./rgb-sound.service   /etc/systemd/system
+
 sudo chown root ./target/release/msi-rgb
 sudo chmod u+s  ./target/release/msi-rgb
 sudo cp         ./target/release/msi-rgb     /usr/local/bin
 sudo cp         ./rgb-cpu.sh                 /usr/local/bin
 sudo cp         ./rgb-net.sh                 /usr/local/bin
 sudo cp         ./rgb-sound.sh               /usr/local/bin
+sudo cp         ./Uss_KelvinEDIT.mp3         /usr/local/share
        
 sudo systemctl disable rgb-cpu
 sudo systemctl disable rgb-net
