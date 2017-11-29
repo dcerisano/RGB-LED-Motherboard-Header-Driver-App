b6da07fca2d043f82dd131d98727b5f1f3d04f03
dcerisano <dcerisano@standard3d.com>
2017-11-29 17:12:19 -0500

    Signed-off-by: dcerisano <dcerisano@standard3d.com>

--- install-sensors.sh
+++ install-sensors.sh
@@ -5,4 +5,4 @@ sudo /sbin/modprobe nct6775 force_id=0xd120 # uncommment if NUVOTON NCT6795D SIO
 sudo sensors-detect
 sudo pwmconfig
 
-# Make a note of which PWM fan you wish to be managed by the rgb-cpu service (if any, but it is cool)
+# Make a note of which PWM fan you wish to be managed by the rgb-cpu service (if any, but it is 'cool')
