454de46d9b8c749f04ba21a49182470a239fe6a9
dcerisano <dcerisano@standard3d.com>
2017-11-29 17:12:07 -0500

    Signed-off-by: dcerisano <dcerisano@standard3d.com>

--- install-sensors.sh
+++ install-sensors.sh
@@ -1,8 +1,8 @@
-# Init sensors and fancontrol
-
 # Install package deps
 sudo apt install xsensors lm-sensors fancontrol
 
 sudo /sbin/modprobe nct6775 force_id=0xd120 # uncommment if NUVOTON NCT6795D SIO chip is not fully suppported by your kernel
 sudo sensors-detect
 sudo pwmconfig
+
+# Make a note of which PWM fan you wish to be managed by the rgb-cpu service (if any, but it is cool)
