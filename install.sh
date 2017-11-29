9e0c82d740cf31fcd14e9037fb15b6e6f67ec664
dcerisano <dcerisano@standard3d.com>
2017-11-29 15:41:41 -0500

    Signed-off-by: dcerisano <dcerisano@standard3d.com>

--- install.sh
+++ install.sh
@@ -1,21 +1,31 @@
-sudo apt install rustc cargo make xsensors lm-sensors fancontrol stress tcpdump
+# If your NUVOTON NCT6795D SIO chip is not suppported by your kernel, this one should work
+sudo /sbin/modprobe nct6775 force_id=0xd120
+  
+# Install package deps
+sudo apt install rustc cargo make xsensors lm-sensors fancontrol stress
 
+# Init sensors and fancontrol
+# Then edit rgb-cpu.sh to point to the fan you want to use as a thruster
 sudo sensors-detect
 sudo pwmconfig
 
+# Install sound effects 
+sudo cp ./standard3d.wav      /usr/local/share
+sudo cp ./vrip360.wav         /usr/local/share
+
+# Install the RGB driver
 cargo build --release
+sudo cp ./target/release/msi-rgb  /usr/local/bin
+
+# Install RGB services
 
 sudo cp ./rgb-cpu.service     /etc/systemd/system
 sudo cp ./rgb-net.service     /etc/systemd/system
 sudo cp ./rgb-sound.service   /etc/systemd/system
+sudo cp ./rgb-cpu.sh          /usr/local/bin
+sudo cp ./rgb-net.sh          /usr/local/bin
+sudo cp ./rgb-sound.sh        /usr/local/bin
 
-sudo cp ./target/release/msi-rgb     /usr/local/bin
-sudo cp ./rgb-cpu.sh                 /usr/local/bin
-sudo cp ./rgb-net.sh                 /usr/local/bin
-sudo cp ./rgb-sound.sh               /usr/local/bin
-sudo cp ./standard3d.wav             /usr/local/share
-sudo cp ./vrip360.wav                /usr/local/share
-       
 sudo systemctl stop    rgb-cpu
 sudo systemctl disable rgb-cpu
 sudo systemctl enable  rgb-cpu
@@ -27,8 +37,8 @@ sudo systemctl enable  rgb-net
 sudo systemctl start   rgb-net
 
 sudo systemctl --user stop    rgb-sound
-systemctl --user disable rgb-sound
-systemctl --user enable  rgb-sound
-systemctl --user start   rgb-sound
+sudo systemctl --user disable rgb-sound
+sudo systemctl --user enable  rgb-sound
+sudo systemctl --user start   rgb-sound
 
 
