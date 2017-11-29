a05cf04c2340159f10f140d777e5a8360cba61a1
dcerisano <dcerisano@standard3d.com>
2017-11-29 16:46:00 -0500

    Signed-off-by: dcerisano <dcerisano@standard3d.com>

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
-# If your NUVOTON NCT6795D SIO chip is not fully suppported by your kernel, this one should work
-sudo /sbin/modprobe nct6775 force_id=0xd120
-  
+
 # Install package deps
 sudo apt install rustc cargo make xsensors lm-sensors fancontrol stress
 
+
+
 # Init sensors and fancontrol
 # Then edit rgb-cpu.sh to point to the fan you want to use as a thruster
+# sudo /sbin/modprobe nct6775 force_id=0xd120 # uncommment if NUVOTON NCT6795D SIO chip is not fully suppported by your kernel
 sudo sensors-detect
 sudo pwmconfig
 
