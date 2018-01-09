16842954eca2dd3dd78f228717e656bd0dcf0083
dcerisano <dcerisano@standard3d.com>
2018-01-08 21:30:49 -0500

    Signed-off-by: dcerisano <dcerisano@standard3d.com>

--- blinkstick-net.sh
+++ blinkstick-net.sh
@@ -1,56 +1,47 @@
 #!/bin/bash
 ###############################################################################
 #
-# RGB SOUND VISUALIZER
+# RGB NET VISUALIZER
 #
-#   Real-time sound volume meter for blinkstick
+#   Real-time network notifications for BlinkStick
 #   D. Cerisano September 4, 2017
-#   Follow blinkstick build instructions in README
+#   Follow msi-rgb build instructions in README
 #
 # Special Requirements:
 #
 #   Pulse Audio
 #
-
+# To build RGB driver:
+#
+#    sudo apt install rustc cargo
+#    cargo --cargo build --release
+#    sudo chown root /target/release/msi-rgb
+#    sudo chmod u+s ./target/release/msi-rgb
+#
 # To test from your git repo: 
 #
-#   ./blinkstick-net.sh
+#   ./rgb-net.sh
 #
 # To run as auto-starting user service (your system must support modern systemctl):
 #   
-#   sudo cp ./blinkstick-net.service          /etc/systemd/user
-#   sudo cp ./blinkstick-net.sh               /usr/local/bin
-#   systemctl --user enable blinkstick-net
-#   systemctl --user start  blinkstick-net
+#   sudo cp ./rgb-net.service          /etc/systemd/system
+#   sudo cp ./rgb-net.sh               /usr/local/bin
+#   systemctl -enable rgb-net
+#   systemctl -start  rgb-net
 #
 # To stop user service:
-#   systemctl --user stop blinkstick-net
+#   systemctl stop rgb-net
 #
 # To disable user service:
-#   systemctl --user disable blinkstick-net
+#   systemctl disable rgb-net
 #
 #
 ###############################################################################
 
 
-# Graceful exit: turn off RGB effect.
-  trap 'USER=root; $rgb_driver --set-led-count 8; exit 1' SIGINT SIGTERM EXIT
-
-     # Delay (~ms*10) - note this loop is performed by SIO, not CPU
-  
-  rgb_driver="/usr/local/bin/blinkstick"  
-
-  $rgb_driver --index 0 FFFFFF
-  $rgb_driver --index 1 FFFFFF
-  $rgb_driver --index 2 FFFFFF
-  $rgb_driver --index 3 FFFFFF
-  $rgb_driver --index 4 FFFFFF
-  $rgb_driver --index 5 FFFFFF
-  $rgb_driver --index 6 FFFFFF
-  $rgb_driver --index 7 FFFFFF
-
 
 # MAIN LOOP
+
 
   tail -F /var/log/apache2/access.log |
   while read line
@@ -64,14 +55,16 @@
      if [[ $line == *"vrip360"* ]]
      then
         aplay /usr/local/share/vrip360.wav &
-        $rgb_driver --index 0 --pulse --duration 250 FF0000 &
+        curl -X GET 'http://dino:5000/?shader=notifier&filename=img/vrip.jpg' > /dev/null 2>&1 
      fi
      
      if [[ $line == *"standard3d"* ]]
      then
         aplay /usr/local/share/standard3d.wav &
-        $rgb_driver --index 7 --pulse --duration 250 8888FF &
+
 
      fi
      
+
   done
+
