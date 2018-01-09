43d25c3310aa030ee03223e0b76dcc164e3d2235
dcerisano <dcerisano@standard3d.com>
2018-01-09 18:58:18 -0500

    Signed-off-by: dcerisano <dcerisano@standard3d.com>

--- rgb-cpu.sh
+++ rgb-cpu.sh
@@ -46,7 +46,7 @@
 export xuser=dcerisano
 export DISPLAY=:0.0
 
-sudo -u $xuser /home/dcerisano/.nvm/versions/node/v8.9.3/bin/node /home/dcerisano/node/node_modules/blinkstick-node/examples/flex_stream/flex_stream_webserver.js &
+sudo -u $xuser /home/dcerisano/.nvm/versions/node/v8.9.3/bin/node /home/dcerisano/node/node_modules/rgb-led-matrix/webserver/rgb_led_matrix_server.js &
 # Graceful exit: turn off RGB effect and set fan to minimum.
   trap '$rgb_driver 0 0 0 -p; echo $pwm_min > $fan; exit 1' SIGINT SIGTERM EXIT
 
