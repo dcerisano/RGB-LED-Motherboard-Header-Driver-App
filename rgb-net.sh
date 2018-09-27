eabaf07bb4d659ec7b399e9ae90f0417a59bc74a
dcerisano <dcerisano@standard3d.com>
2018-09-27 00:56:06 -0400

    update
    Signed-off-by: dcerisano <dcerisano@standard3d.com>

--- rgb-net.sh
+++ rgb-net.sh
@@ -1,44 +1,4 @@
 #!/bin/bash                                                                                                                                                                                                     
-###############################################################################                                                                                                                                 
-#                                                                                                                                                                                                               
-# RGB SOUND VISUALIZER                                                                                                                                                                                          
-#                                                                                                                                                                                                               
-#   Real-time sound volume meter for msi-rgb                                                                                                                                                                    
-#   D. Cerisano September 4, 2017                                                                                                                                                                               
-#   Follow msi-rgb build instructions in README                                                                                                                                                                 
-#                                                                                                                                                                                                               
-# Special Requirements:                                                                                                                                                                                         
-#                                                                                                                                                                                                               
-#   Pulse Audio                                                                                                                                                                                                 
-#                                                                                                                                                                                                               
-# To build RGB driver:
-#
-#    sudo apt install rustc cargo
-#    cargo --cargo build --release
-#    sudo chown root /target/release/msi-rgb
-#    sudo chmod u+s ./target/release/msi-rgb
-#
-# To test from your git repo: 
-#
-#   ./rgb-net.sh
-#
-# To run as auto-starting user service (your system must support modern systemctl):
-#   
-#   sudo cp ./rgb-net.service          /etc/systemd/user
-#   sudo cp ./target/release/msi-rgb   /usr/local/bin
-#   sudo cp ./rgb-net.sh               /usr/local/bin
-#   sudo chmod u+s /usr/local/bin/msi-rgb
-#   systemctl --user enable rgb-net
-#   systemctl --user start  rgb-net
-#
-# To stop user service:
-#   systemctl --user stop rgb-net
-#
-# To disable user service:
-#   systemctl --user disable rgb-net
-#
-#
-###############################################################################
 
 
 #  Check if running as user service
@@ -61,23 +21,17 @@
   
      if [[ $line == *"vrip360"* ]]
      then
-        aplay /usr/local/share/vrip360.wav &
-        curl -X GET 'http://praxis:8080?method=set_shader_req&shader=notifier&options.image=vrip.png' > /dev/null 2>&1 
-        curl -X GET 'http://kronos:8080?method=set_shader_req&shader=notifier&options.image=vrip.png' > /dev/null 2>&1 
+         curl -X GET 'http://kronos:8080?method=set_shader_req&shader=notifier&options.image=vrip.png' > /dev/null 2>&1 
      fi
      
      if [[ $line == *"standard3d"* ]]
      then
-        aplay /usr/local/share/standard3d.wav &
-        curl -X GET 'http://praxis:8080?method=set_shader_req&shader=notifier&options.image=s3d.png' > /dev/null 2>&1 
-        curl -X GET 'http://kronos:8080?method=set_shader_req&shader=notifier&options.image=s3d.png' > /dev/null 2>&1 
+         curl -X GET 'http://kronos:8080?method=set_shader_req&shader=notifier&options.image=s3d.png' > /dev/null 2>&1 
      fi
      
           if [[ $line == *"rgbify"* ]]
      then
-        aplay /usr/local/share/rgbify.wav &
-        curl -X GET 'http://praxis:8080?method=set_shader_req&shader=notifier&options.image=rgbify.png' > /dev/null 2>&1 
-        curl -X GET 'http://kronos:8080?method=set_shader_req&shader=notifier&options.image=rgbify.png' > /dev/null 2>&1 
+         curl -X GET 'http://kronos:8080?method=set_shader_req&shader=notifier&options.image=rgbify.png' > /dev/null 2>&1 
      fi
      
   done
